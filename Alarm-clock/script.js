160a7988f5e0424f8b2767e99f6eb88cd979e4ec
mudit108-code <127120092+mudit108-code@users.noreply.github.com>
2024-01-08 12:24:25 +0530

    Update script.js

--- Alarm-clock/script.js
+++ Alarm-clock/script.js
@@ -1,72 +1,69 @@
-const currentTime = document.querySelector("h1"),
-content = document.querySelector(".content"),
-selectMenu = document.querySelectorAll("select"),
-setAlarmbtn = document.querySelector("button");
+const currentTime = document.querySelector("h1");
+const content = document.querySelector(".content");
+const selectMenu = document.querySelectorAll("select");
+const setAlarmBtn = document.querySelector("button");
 
 let alarmTime, isAlarmSet = false;
-ringtone = new Audio('./Files/ringtone.mp3')
+const ringtone = new Audio('./Files/ringtone.mp3');
 
-for(let i = 12; i > 0; i--){
-    i = i < 10 ? "0" + i : i;
-    let option = `<option value="${i}">${i}</option>`;
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+
+function generateOptions(start, end, step, selector) {
+    for (let i = start; i >= end; i -= step) {
+        const value = i < 10 ? "0" + i : i;
+        const option = `<option value="${value}">${value}</option>`;
+        selector.firstElementChild.insertAdjacentHTML("afterend", option);
+    }
 }
 
-for(let i = 59; i >= 0; i--){
-    i = i < 10 ? "0" + i : i;
-    let option = `<option value="${i}">${i}</option>`;
-    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
-}
+// Generate options for hours, minutes, and AM/PM
+generateOptions(12, 1, 1, selectMenu[0]);
+generateOptions(59, 0, 1, selectMenu[1]);
+generateOptions(1, 0, 1, selectMenu[2]);
 
-for(let i = 2; i > 0; i--){
-    let ampm = i == 1 ? "AM" : "PM"
-    let option = `<option value="${ampm}">${ampm}</option>`;
-    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
-}
 
 setInterval(() => {
-    let date = new Date(),
-    h = date.getHours(),
-    m = date.getMinutes(),
-    s = date.getSeconds(),
-    ampm = 'AM';
+    const date = new Date();
+    let h = date.getHours(),
+        m = date.getMinutes(),
+        s = date.getSeconds(),
+        ampm = 'AM';
 
-    if(h >= 12){
-        h = h - 12;
+    if (h >= 12) {
+        h -= 12;
         ampm = "PM";
     }
 
-    h = h == 0 ? h = 12 : h;
-
+    h = (h === 0) ? 12 : h;
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
 
-    currentTime.innerText = `${h}:${m}:${s} ${ampm}`
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
 
-    if( alarmTime == `${h}:${m} ${ampm}`){
+    if (alarmTime === `${h}:${m} ${ampm}`) {
         ringtone.play();
         ringtone.loop = true;
     }
 }, 1000);
 
-function setAlarm(){
-    if(isAlarmSet){
+
+function setAlarm() {
+    if (isAlarmSet) {
         alarmTime = "";
         ringtone.pause();
         content.classList.remove("disable");
-        setAlarmbtn.innerText = "Set alarm";
-        return isAlarmSet = false;
+        setAlarmBtn.innerText = "Set alarm";
+        isAlarmSet = false;
+    } else {
+        const time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+        if (time.includes("Hour") || time.includes("Minutes") || time.includes("AM/PM")) {
+            return alert("Please select a valid time to set the alarm!");
+        }
+        alarmTime = time;
+        isAlarmSet = true;
+        content.classList.add("disable");
+        setAlarmBtn.innerText = "Clear alarm";
     }
-
-    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`
-    if(time.includes("Hour") || time.includes("Minutes") || time.includes("AM/PM")){
-        return alert("Please select a valid time to set the alarm!")
-    }
-    alarmTime = time;
-    isAlarmSet = true;
-    content.classList.add("disable");
-    setAlarmbtn.innerText = "Clear alarm";
 }
 
-setAlarmbtn.addEventListener("click", setAlarm);
+setAlarmBtn.addEventListener("click", setAlarm);
